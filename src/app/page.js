2f36763f0b01c68c24e4168c3cd0a06446cb7068
Belinda Marion Kobusingye <46527380+Codebmk@users.noreply.github.com>
2024-07-08 07:09:11 +0300

    fix responsiveness

--- src/app/page.js
+++ src/app/page.js
@@ -10,6 +10,7 @@ import Unhealthy from "@icons/Unhealthy";
 import UnhealthySG from "@icons/UnhealthySG";
 import VeryUnhealthy from "@icons/VeryUnhealthy";
 import UnknownAQ from "@icons/Invalid";
+import WindIcon from "@icons/WindIcon";
 
 const BoxWrapper = ({ children }) => {
   const window = useWindowSize();
@@ -48,12 +49,12 @@ const Footer = () => {
 
 const AirQualityDetails = (site) => {
   const window = useWindowSize();
-  const screenWidth = window.width;
+  const screenWidth = Math.floor(window.width);
   const [selectedSite, setSelectedSite] = useState(null);
 
   const getAQIMessage = (aqi_category, healthTip) => {
     if (aqi_category !== "") {
-      return `Air quality is ${aqi_category}. ${healthTip}`;
+      return `Air quality is ${aqi_category}`;
     } else {
       return "";
     }
@@ -80,8 +81,8 @@ const AirQualityDetails = (site) => {
 
     return (
       <AirQualityIcon
-        width={screenWidth >= 2160 ? "480" : "205.36"}
-        height={screenWidth >= 2160 ? "480" : "205.36"}
+        width={`${screenWidth * 0.18}`}
+        height={`${screenWidth * 0.18}`}
       />
     );
   };
@@ -92,19 +93,20 @@ const AirQualityDetails = (site) => {
     }
   }, [site]);
 
+  console.log(screenWidth * 0.04)
+
   return (
     <div
       className={`${
-        screenWidth >= 2160 ? "h-full" : "h-4/5 min-h-[720px]"
-      } w-full rounded-3xl px-[5%] pt-[3%] bg-blue-950 border-t-2 border-blue-950 overflow-x-hidden relative`}
+        screenWidth >= 2160 ? "h-full" : "h-4/5 min-h-[620px]"
+      } w-full rounded-3xl px-[5%] pt-[3%] bg-blue-950 border-t-2 border-blue-950 overflow-hidden relative`}
     >
       <div className="w-full flex-col justify-start items-start gap-1 flex">
         <div className="flex-col justify-start items-start flex">
           <div className="flex justify-start items-start divide-x-2 divide-white">
             <div
-              className={`text-neutral-50 ${
-                screenWidth <= 2160 ? "text-[20px]" : "text-4xl"
-              } font-medium font-['Inter'] pr-5 leading-tight`}
+              className={`text-neutral-50 font-medium font-['Inter'] pr-5 leading-tight`}
+              style={{fontSize: `${screenWidth * 0.015}px`}}
             >
               {new Date().toLocaleDateString([], {
                 weekday: "long",
@@ -114,9 +116,8 @@ const AirQualityDetails = (site) => {
               })}
             </div>
             <div
-              className={`text-neutral-50 ${
-                screenWidth <= 2160 ? "text-[20px]" : "text-4xl"
-              } font-medium font-['Inter'] pl-3 leading-tight`}
+              className={`text-neutral-50 font-medium font-['Inter'] leading-tight pl-5`}
+              style={{fontSize: `${screenWidth * 0.015}px`}}
             >
               {new Date().toLocaleTimeString([], {
                 hour: "numeric",
@@ -125,34 +126,37 @@ const AirQualityDetails = (site) => {
             </div>
           </div>
           <div
-            className={`text-neutral-50 ${
-              screenWidth >= 2160 ? "text-[160px]" : "text-[64px]"
-            } font-bold font-['Inter'] leading-tight`}
+            className={`text-neutral-50 font-bold font-['Inter'] leading-tight`}
+            style={{fontSize: `${screenWidth * 0.05}px`}}
           >
             Air Quality
           </div>
         </div>
-        <div className="w-full justify-between items-start flex py-[1%]">
+        <div className="w-full justify-between items-center flex py-[1%]">
           <div className="flex-col justify-between items-start w-full">
             <div className="self-stretch flex-col justify-start items-start flex">
               <div className="flex justify-start items-center gap-4">
                 <div
-                  className={`${
-                    screenWidth >= 2160 ? "w-16 h-16" : "w-7 h-7"
-                  } p-1 bg-blue-100 rounded-full justify-center items-center flex`}
+                  className="p-1 bg-blue-100 rounded-full flex justify-center items-center"
+                  style={{
+                    width: `${screenWidth * 0.04}px`,
+                    height: `${screenWidth * 0.04}px`,
+                  }}
                 >
-                  <Image
+                  {/* <Image
                     src="/wind.png"
                     alt="Image X"
-                    width={screenWidth >= 2160 ? 44.7 : 22.35}
-                    height={screenWidth >= 2160 ? 44.7 : 22.35}
+                    width={screenWidth * 0.04}
+                    height={screenWidth * 0.04}
                     style={{ objectFit: "contain" }}
-                  />
+                  /> */}
+                  <WindIcon
+                  width={`${screenWidth * 0.035}`}
+                  height={`${screenWidth * 0.035}`} />
                 </div>
                 <div
-                  className={`text-right text-neutral-50 ${
-                    screenWidth <= 2160 ? "text-[24px]" : "text-[44px]"
-                  } font-medium font-['Inter'] leading-tight`}
+                  className={`text-right text-neutral-50 font-medium font-['Inter'] leading-tight`}
+                  style={{fontSize: `${screenWidth * 0.03}px`}}
                 >
                   PM2.5
                 </div>
@@ -160,11 +164,8 @@ const AirQualityDetails = (site) => {
             </div>
             <div className="flex justify-start items-baseline gap-[11.18px]">
               <div
-                className={`text-center text-orange-400 ${
-                  screenWidth >= 2160
-                    ? "text-[151.76px] leading-[151.76px]"
-                    : "text-[64px] leading-tight"
-                } font-extrabold font-['Inter']`}
+                className={`text-center text-orange-400 font-extrabold font-['Inter']`}
+                style={{fontSize: `${screenWidth * 0.08}px`}}
               >
                 {selectedSite && selectedSite.pm2_5
                   ? selectedSite.pm2_5.value.toFixed(2)
@@ -172,20 +173,14 @@ const AirQualityDetails = (site) => {
               </div>
               <div className="text-right">
                 <span
-                  className={`text-orange-400 ${
-                    screenWidth >= 2160
-                      ? "text-[67.05px] leading-[89.40px]"
-                      : "text-3xl leading-tight"
-                  } font-bold font-['Inter']`}
+                  className={`text-orange-400 font-bold font-['Inter']`}
+                  style={{fontSize: `${screenWidth * 0.03}px`}}
                 >
                   μg/m
                 </span>
                 <span
-                  className={`text-orange-400 ${
-                    screenWidth >= 2160
-                      ? "text-[55.88px] leading-[78.23px]"
-                      : "text-3xl leading-tight"
-                  } font-medium font-['Inter']`}
+                  className={`text-orange-400 font-medium font-['Inter']`}
+                  style={{fontSize: `${screenWidth * 0.03}px`}}
                 >
                   3
                 </span>
@@ -194,31 +189,26 @@ const AirQualityDetails = (site) => {
           </div>
           {selectedSite && selectedSite.pm2_5 && (
             <div
-              className={`${
-                screenWidth >= 2160
-                  ? "w-[500px] h-[500px]"
-                  : "w-[205.36px] h-[205.36px]"
-              } justify-center items-center flex`}
+              className={`${`w-[${screenWidth * 0.20}px] h-[${screenWidth * 0.20}px]`} justify-center items-center flex`}
             >
               {getAQIIcon(selectedSite.pm2_5.value)}
             </div>
           )}
         </div>
       </div>
+      <div className="absolute bottom-[7%] right-[5%] left-[5%]">
       <div
-        className={`max-w-[70%] w-full text-neutral-50 ${
-          screenWidth >= 2160 ? "text-5xl" : "text-[28px]"
-        } font-semibold font-['Inter']`}
+        className={`max-w-[60%] w-full text-neutral-50 leading-snug font-semibold font-['Inter']`}
+        style={{fontSize: `${screenWidth * 0.02}px`}}
       >
-        {selectedSite && selectedSite.pm2_5 && selectedSite.pm2_5.aqi_category
+        {selectedSite && selectedSite.aqi_category
           ? getAQIMessage(
-              selectedSite.pm2_5.aqi_category,
-              selectedSite.pm2_5.health_tip[0]
+              selectedSite.aqi_category,
+              selectedSite.health_tips[0].description
             )
           : ""}
       </div>
-      <div className="absolute bottom-[2%] right-[5%] left-[5%]">
-        <hr className="pb-4 mt-4" />
+        <hr className="pb-4 mt-5" />
         <div className="h-[99.60px] flex-col justify-start items-start gap-4 flex">
           <div className="justify-center items-center gap-5 inline-flex">
             <div>
@@ -231,11 +221,8 @@ const AirQualityDetails = (site) => {
               />
             </div>
             <div
-              className={`text-neutral-50 ${
-                screenWidth >= 2160
-                  ? "text-[62.10px] leading-[79.85px]"
-                  : "text-[40px]"
-              } font-bold font-['Inter']`}
+              className={`text-neutral-50 leading-loose font-bold font-['Inter']`}
+              style={{fontSize: `${screenWidth * 0.02}px`}}
             >
               {selectedSite && selectedSite.siteDetails
                 ? selectedSite.siteDetails.search_name
